2dba179fec87aefc70a5b574efd915917102fba2
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2025-04-03 08:23:10 +0200

    script.sh: add output argument

--- script.sh
+++ script.sh
@@ -3,8 +3,9 @@
 DOCKER_IMAGE=rb
 version=$1
 size=$2
+output=$3
 docker build -t ${DOCKER_IMAGE} .
 { time \
       docker run --net=host -v $(pwd):/home ${DOCKER_IMAGE} python3 \
-      main.py --version ${version} --n ${size} --generate-config
+      main.py --version ${version} --n ${size} --generate-config --output ${output}
 } 2>${project}.duration
