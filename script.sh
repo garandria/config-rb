d0a724f78cf579a17a693fc9b51f21472efecc2c
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2025-04-23 19:17:27 +0200

    script: thread parameter for experiment machine

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ project=$4
 docker build -t ${DOCKER_IMAGE} .
 { time \
       docker run --net=host -v $(pwd):/home ${DOCKER_IMAGE} python3 \
-      main.py --version ${version} --n ${size} --generate-config --output ${output}
+      main.py --version ${version} --n ${size} --generate-config --threads 64 --output ${output}
 } 2>${project}.duration
